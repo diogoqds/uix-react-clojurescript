251ecdc3d8c6d0c1c10a50e48b275f234684ebe6
Diogo <diogo.qds97@gmail.com>
2023-10-09 07:01:35 -0300

    test antd and react-router-dom

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,110 +1,62 @@
 (ns app.core
   (:require
-    [cljs.spec.alpha :as s]
-    [clojure.edn :as edn]
     [uix.core :as uix :refer [defui $]]
-    [uix.dom]))
+    [uix.dom]
+    ["react-router-dom" :as router]
+    ["antd" :as antd]))
 
-(defui header []
-  ($ :header.app-header
-    ($ :img {:src "https://raw.githubusercontent.com/pitch-io/uix/master/logo.png"
-             :width 32})))
+(def context (uix.core/create-context {}))
 
-(defui footer []
-  ($ :footer.app-footer
-    ($ :small "made with "
-              ($ :a {:href "https://github.com/pitch-io/uix"}
-                    "UIx"))))
+(defui context-provider
+  [{:keys [children]}]
+  (let [[counter set-counter] (uix.core/use-state 0)]
 
-(defui text-field [{:keys [on-add-todo]}]
-  (let [[value set-value!] (uix/use-state "")]
-    ($ :input.text-input
-      {:value value
-       :placeholder "Add a new todo and hit Enter to save"
-       :on-change (fn [^js e]
-                    (set-value! (.. e -target -value)))
-       :on-key-down (fn [^js e]
-                      (when (= "Enter" (.-key e))
-                        (set-value! "")
-                        (on-add-todo #(assoc % (js/Date.now) {:text value
-                                                              :status :unresolved}))))})))
+    (uix.core/use-effect
+     (fn []
+       (set-counter (fn [x] (inc x))))
+     [])
+    ($ (.-Provider context) {:value {:title "Opa" :counter counter :set-counter set-counter}}
+       children)))
 
-(defui editable-text [{:keys [text text-style on-done-editing]}]
-  (let [[editing? set-editing!] (uix/use-state false)
-        [editing-value set-editing-value!] (uix/use-state "")]
-    (if editing?
-      ($ :input.todo-item-text-field
-        {:value editing-value
-         :auto-focus true
-         :on-change (fn [^js e]
-                      (set-editing-value! (.. e -target -value)))
-         :on-key-down (fn [^js e]
-                        (when (= "Enter" (.-key e))
-                          (set-editing-value! "")
-                          (set-editing! false)
-                          (on-done-editing editing-value)))})
-      ($ :span.todo-item-text
-        {:style text-style
-         :on-click (fn [_]
-                     (set-editing! true)
-                     (set-editing-value! text))}
-        text))))
+(defui title
+  []
+  (let [context (uix.core/use-context context)]
+    ($ :<>
+       ($ (.-Title antd/Typography) (:title context))
+       ($ router/Link {:to "/counter"} "Counter")
+       ($ router/Link {:to "/button"} "Button"))))
 
-(s/def :todo/text string?)
-(s/def :todo/status #{:unresolved :resolved})
+(defui counter
+  []
+  (let [context (uix.core/use-context context)]
+    ($ :<>
+       ($ :h2 (:counter context))
+       ($ router/Link {:to "/"} "Home")
+       ($ router/Link {:to "/button"} "Button"))))
 
-(s/def :todo/item
-  (s/keys :req-un [:todo/text :todo/status]))
+(defui button
+  []
+  (let [context (uix.core/use-context context)]
+    ($ :<>
+       ($ router/Link {:to "/"} "Home")
+       ($ router/Link {:to "/counter"} "Counter")
+       ($ :h2 (:counter context))
+       ($ antd/Button {:on-click (fn [] ((:set-counter context) inc))}
+          "Click me"))))
 
-(defui todo-item
-  [{:keys [created-at text status on-update-todos] :as props}]
-  {:pre [(s/valid? :todo/item props)]}
-  ($ :.todo-item
-    {:key created-at}
-    ($ :input.todo-item-control
-      {:type :checkbox
-       :checked (= status :resolved)
-       :on-change (fn [_]
-                    (on-update-todos #(update-in % [created-at :status] {:unresolved :resolved
-                                                                         :resolved :unresolved})))})
-    ($ editable-text
-      {:text text
-       :text-style {:text-decoration (when (= :resolved status) :line-through)}
-       :on-done-editing (fn [value]
-                          (on-update-todos #(assoc-in % [created-at :text] value)))})
-    ($ :button.todo-item-delete-button
-      {:on-click (fn [_]
-                   (on-update-todos #(dissoc % created-at)))}
-      "×")))
+(defui router-provider
+  []
+  ($ (.-BrowserRouter router)
+     ($ (.-Routes router)
+        ($ (.-Route router) {:path "/" :exact true :element ($ title)})
+        ($ (.-Route router) {:path "/counter" :element ($ counter)})
+        ($ (.-Route router) {:path "/button" :element ($ button)}))))
 
-(defn use-persistent-state
-  "Loads initial state from local storage and persists every updated state value
-  Returns a tuple of the current state value and an updater function"
-  [store-key initial-value]
-  (let [[value set-value!] (uix/use-state initial-value)]
-    (uix/use-effect
-      (fn []
-        (let [value (edn/read-string (js/localStorage.getItem store-key))]
-          (set-value! #(into % value))))
-      [store-key])
-    (uix/use-effect
-      (fn []
-        (js/localStorage.setItem store-key (str value)))
-      [value store-key])
-    [value set-value!]))
-
-
-(defui app []
-  (let [[todos set-todos!] (use-persistent-state "uix-starter/todos" (sorted-map-by >))]
-    ($ :.app
-      ($ header)
-      ($ text-field {:on-add-todo set-todos!})
-      (for [[created-at todo] todos]
-        ($ todo-item
-          (assoc todo :created-at created-at
-                      :key created-at
-                      :on-update-todos set-todos!)))
-      ($ footer))))
+(defui app
+  []
+  ($ context-provider
+     ($ router-provider
+        ($ :h1 "oi"))))
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
